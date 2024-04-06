a4d9af375987e14f657459bfd0771879eb1b54fa
Nathan Gavenski <nathangavenski@gmail.com>
2024-04-06 20:51:59 +0100

    Fix harpoon and config for tex

--- after/plugin/tex.lua
+++ after/plugin/tex.lua
@@ -1,3 +1,13 @@
 --So it is the same as <leader> instead of \
+vim.g.vimtex_compiler_method = "latexmk"
 vim.g.maplocalleader = " "
 vim.g.vimtex_view_method = "zathura"
+vim.g.vimtex_compiler_latexmk = {
+    options = {
+        '-verbose',
+        '-file-line-error',
+        '-synctex=1',
+        '-interaction=nonstopmode',
+        '-shell-escape',
+    },
+}
--- lua/nathan/packer.lua
+++ lua/nathan/packer.lua
@@ -7,19 +7,27 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  use {"nvim-lua/plenary.nvim"}
+
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  -- or                            , branch = '0.1.x',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+ use {
+     "ThePrimeagen/harpoon",
+     branch = "harpoon2",
+     requires = { {"nvim-lua/plenary.nvim"} }
+ }
 
  use({ "catppuccin/nvim", as = "catppuccin" })
 
  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
  use('nvim-treesitter/playground')
- use('theprimeagen/harpoon')
  use('mbbill/undotree')
+
  use('tpope/vim-fugitive')
+
  use('lervag/vimtex')
  use("m4xshen/smartcolumn.nvim")
 
